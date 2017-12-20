c99ce0fc420772cf159b55ce04d6626745f6aaa8
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-20 14:20:59 +0000

    add class skeleton basic --  MVP basic --> modifica las clases 'pr-off' por 'pr-on', y viceversa, cuando llamamos a la clase SkeletonPreloader

--- skeletonPreloader.js
+++ skeletonPreloader.js
@@ -7,23 +7,32 @@
  * Daniel ...
  */
 class SkeletonPreloader {
-    typeAnimated = new String;
-    animatedOn = new Boolean;
-    elementsOn = new Array;
-    elementsOff = new Array;
     constructor() {
-        LoadElements();
-        AddClass();
+        this.animatedOn = true;
+        this.typeAnimated = 'fade-in';
+        this.elementsOn = [];
+        this.elementsOff = [];
+        this.loadElements();
+        this.finish();
     };
     loadElements() {
-        this.elementsOn = document.querySelectorAll('pr-on');
-        this.elementsOff = document.querySelectorAll('pr-off');
+        this.elementsOn = document.querySelectorAll('.pr-on');
+        this.elementsOff = document.querySelectorAll('.pr-off');
+        console.log(this.elementsOn, this.elementsOff);
+        return this.elementsOn, this.elementsOff;
     };
-    replaceClass(classToReplace, classReplace) {
-        this.elements.classList.replace(classToReplace, classReplace);
+    addClass(elements, classToAdd) {
+        for (let i = 0; i < elements.length; i++)
+            elements[i].classList.add(classToAdd);
     };
-    finish(elements) {
-        replaceClass('pr-on', 'pr-off');
-        replaceClass('pr-off', 'pr-on');
+    replaceClass(elements, classToReplace, classReplace) {
+        console.log(elements);
+        if (elements.length == 0) return;
+        for (let i = 0; i < elements.length; i++)
+            elements[i].classList.replace(classToReplace, classReplace);
+    };
+    finish() {
+        this.replaceClass(this.elementsOn, 'pr-on', 'pr-off');
+        this.replaceClass(this.elementsOff, 'pr-off', 'pr-on');
     };
 };
