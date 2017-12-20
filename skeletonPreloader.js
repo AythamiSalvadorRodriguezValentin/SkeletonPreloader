137c6ab4fea6d86a528e73e93866469b575a00ea
Aythami Rodriguez Valentin <aythami94srv@outlook.com>
2017-12-20 13:48:24 +0000

    add class skeleton-preloader

--- skeletonPreloader.js
+++ skeletonPreloader.js
@@ -1,1 +1,29 @@
-class SkeletonPreloader () {}
+// Clase constructora skeleton-preloader.
+/**
+ * - - SkeletonPreloader - -
+ * Autor AythamiSalvador Rodriguez Valentin
+ * Ruben ...
+ * Rebeca ...
+ * Daniel ...
+ */
+class SkeletonPreloader {
+    typeAnimated = new String;
+    animatedOn = new Boolean;
+    elementsOn = new Array;
+    elementsOff = new Array;
+    constructor() {
+        LoadElements();
+        AddClass();
+    };
+    loadElements() {
+        this.elementsOn = document.querySelectorAll('pr-on');
+        this.elementsOff = document.querySelectorAll('pr-off');
+    };
+    replaceClass(classToReplace, classReplace) {
+        this.elements.classList.replace(classToReplace, classReplace);
+    };
+    finish(elements) {
+        replaceClass('pr-on', 'pr-off');
+        replaceClass('pr-off', 'pr-on');
+    };
+};
